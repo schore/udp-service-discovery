f0c5afc886ac34a238236eb1cd441b5d92efe313
schore <schore@users.noreply.github.com>
2014-05-30 09:56:27 +0200

    Added response to the listener

--- udplistener.py
+++ udplistener.py
@@ -1,3 +1,4 @@
+"""Small Test Server"""
 import discrequestobj
 import socket
 
@@ -6,12 +7,15 @@ def server(port):
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.bind(('', port))
     discrequest = discrequestobj.DiscObject(0x43, "III")
+    discresponse = discrequestobj.DiscObject(43, "III")
+    response = discresponse.encode(11, 5321)
     while True:
         data, addr = sock.recvfrom(
                 discrequest.get_size())
         disc_type = discrequest.decode(data)
         print "received " + data +" from " + addr[0]+ " " + str(addr[1])
         print "Type" + str(disc_type)
+        sock.sendto(response, (addr[0], disc_type[1]))
 
 
 if __name__ == '__main__':
