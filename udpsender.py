9c9a70c0c18d1a9a232f2a1f0074b825457873c4
schore <schore@users.noreply.github.com>
2014-06-14 15:14:18 +0200

    service discovery seems to work

--- udpsender.py
+++ udpsender.py
@@ -5,24 +5,23 @@ import staticenumerations
 import zmq
 import servicelist
 
-servlist = servicelist.ServiceList()
-def discovery(ip, port):
+def discovery(ip, port, ty):
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     discrequ = discrequestobj.DiscObject(0x43, "III")
-    serv = staticenumerations.SERVICE_TYPE.get_numb('servdisc')
+    serv = staticenumerations.SERVICE_TYPE.get_numb(ty)
     ctx = zmq.Context()
     zmq_sock = ctx.socket(zmq.REP)
-    while True:
-        aport = zmq_sock.bind_to_random_port("tcp://*")
-        data_stream = discrequ.encode(serv, aport)
-        sock.sendto(data_stream, (ip, port))
-        print "Sending test" + str(aport)
-        rxob = zmq_sock.recv_pyobj()
-        zmq_sock.send("rx ok")
-        print rxob
-        print type(rxob)
-        import ipdb; ipdb.set_trace() # BREAKPOINT
-        time.sleep(1)
+    aport = zmq_sock.bind_to_random_port("tcp://*")
+    data_stream = discrequ.encode(serv, aport)
+    sock.sendto(data_stream, (ip, port))
+    rxob = zmq_sock.recv_pyobj()
+    zmq_sock.send("rx ok")
+    return rxob
 
 if __name__ == '__main__':
-    discovery("127.0.0.1", 5555)
+    rx = discovery("127.0.0.1", 5555, 'servdisc')
+    for i in rx:
+        i.print_list()
+    rx = discovery("127.0.0.1", 5555, 'red')
+    for i in rx:
+        i.print_list()
